61b72db09a841851e0fa11d70e14dc05de5fd19d
Josue <35145882+NaturalDevCR@users.noreply.github.com>
2023-12-13 21:46:48 -0600

    RollBack to JS
    Typescript support requires some work, if somebody can help, go ahead!

--- test.js
+++ test.js
@@ -1,14 +1,15 @@
-const indicadoresEconomicosBCCR = require('./index');
+
+const BCCR = require('./index');
 
 const email = 'YOUR_EMAIL';
 const token = 'YOUR_TOKEN';
 
-indicadoresEconomicosBCCR(email, token).then((data:any) => {
+BCCR(email, token).then((data) => {
     console.log("Compra y Venta del día de hoy:");
     console.log(data);
 });
 
-indicadoresEconomicosBCCR(email, token, '20/09/2018', '20/09/2018').then((data:any) => {
+BCCR(email, token, '20/09/2018', '20/09/2018').then((data) => {
     console.log("Compra y Venta por fecha:");
     console.log(data)
 });
